b8ba720de5c10f2ba19c00c8de180e7a5b43d1ed
Samuel0n <sam.uel20@outlook.com>
2022-09-14 16:05:25 -0300

    Config Firebase

--- src/app/Components/list-empleados/list-empleados.component.ts
+++ src/app/Components/list-empleados/list-empleados.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-list-empleados',
-  templateUrl: './list-empleados.component.html',
-  styleUrls: ['./list-empleados.component.css']
+  selector: "app-list-empleados",
+  templateUrl: "./list-empleados.component.html",
+  styleUrls: ["./list-empleados.component.css"],
 })
-export class ListEmpleadosComponent implements OnInit {
-  
-  constructor() { }
-
-  ngOnInit(): void {
+export class ListEmpleadosComponent implements OnInit {  
+  items: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('items').valueChanges();
   }
 
+  ngOnInit(): void {}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCS6IPSkKvrNHrxL1w72od6VognxqU6TkI",
-    authDomain: "empleadosapp-bc787.firebaseapp.com",
-    projectId: "empleadosapp-bc787",
-    storageBucket: "empleadosapp-bc787.appspot.com",
-    messagingSenderId: "539386155938",
-    appId: "1:539386155938:web:7256fdb0f4b63ee58298fb",
+    projectId: 'empleadosapp-bc787',
+    appId: '1:539386155938:web:7256fdb0f4b63ee58298fb',
+    storageBucket: 'empleadosapp-bc787.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCS6IPSkKvrNHrxL1w72od6VognxqU6TkI',
+    authDomain: 'empleadosapp-bc787.firebaseapp.com',
+    messagingSenderId: '539386155938',
   },
 };
