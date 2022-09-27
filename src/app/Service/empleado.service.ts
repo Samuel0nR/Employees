70acae3bd8cf1a723cb9e019411d5106b985ed69
Samuel0n <sam.uel20@outlook.com>
2022-09-27 11:18:32 -0300

    Actualizacion pag

--- src/app/Service/empleado.service.ts
+++ src/app/Service/empleado.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EmpleadoService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  getEmpleados(): Observable<any> {
+    return this.firestore.collection("empleado", ref => ref.orderBy('fechaCreacion','asc')).snapshotChanges();
+  }
+
+  agregarEmpleado(empleado: any): Promise<any> {
+    return this.firestore.collection("empleado").add(empleado);
+  }
+
+  borrarEmpleado(id: string): Promise<any> {
+    return this.firestore.collection("empleado").doc(id).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { ListEmpleadosComponent } from "./Components/list-empleados/list-empleados.component";
 import { CreateEmpleadoComponent } from "./Components/create-empleado/create-empleado.component";
@@ -22,12 +26,15 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     NavbarComponent,
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
+    BrowserAnimationsModule, 
     AppRoutingModule, 
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
